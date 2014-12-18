1ed5d4ce9d15ba46e6d75aece0e56514be64dab9
fuzzyanalysis <fuzzyanalysis@gmail.com>
2014-12-19 01:09:17 +1100

    Shannon - fixing darn GITHUB issues.

---
+++ src/test/java/com/datasciencebox/editor/BasicViewTest.java
@@ -0,0 +1,16 @@
+package com.datasciencebox.editor;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BasicViewTest {
+	@Test
+	public void multiplicationOfZeroIntegersShouldReturnZero() {
+
+	   // MyClass is tested
+	   // Tests
+	   Assert.assertEquals("10 x 0 must be 0", 0, 10*0);
+	   Assert.assertEquals("0 x 10 must be 0", 0, 0*10);
+	   Assert.assertEquals("0 x 0 must be 0", 0, 0*0);
+	 } 
+}

976f9e50853d3dc04099365b83983e996cbdf780
donkey <donkey@donkey-B85M-HD3>
2014-12-29 01:50:30 +1100

    Shannon - Added gojs diagrams to vertical tabs

---
+++ src/main/java/com/datasciencebox/editor/TabbedView.java
@@ -0,0 +1,35 @@
+package com.datasciencebox.editor;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.TabChangeEvent;
+import org.primefaces.event.TabCloseEvent;
+ 
+@ManagedBean
+public class TabbedView {
+     
+    private List<String> cars;
+ 
+    @PostConstruct
+    public void init() {
+
+    }
+     
+    public List<String> getCars() {
+        return cars;
+    }
+     
+    public void onTabChange(TabChangeEvent event) {
+        FacesMessage msg = new FacesMessage("Tab Changed", "Active Tab: " + event.getTab().getTitle());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+         
+    public void onTabClose(TabCloseEvent event) {
+        FacesMessage msg = new FacesMessage("Tab Closed", "Closed tab: " + event.getTab().getTitle());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+}

b7181d46a70018a0aeff7ed63b0aaa9f698de8e7
donkey <fuzzyanalysis@gmail.com>
2015-04-12 22:03:25 +1000

    Frank - messing around with GoJS, Layouts, and other fun stuff in Primefaces.

--- src/main/java/com/datasciencebox/editor/ButtonBean.java
+++ src/main/java/com/datasciencebox/editor/ButtonBean.java
@@ -1,27 +1,70 @@
 package com.datasciencebox.editor;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.Serializable;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
+import javax.mail.Session;
 import javax.servlet.http.HttpServletRequest;
 
-@ManagedBean
+import org.apache.commons.io.IOUtils;
+import org.primefaces.json.JSONException;
+import org.primefaces.json.JSONObject;
+
+@ManagedBean(name = "buttonBean")
 @ViewScoped
 public class ButtonBean implements Serializable {
 
 	public String host;
-	private String text = "Starting text";//getter + setter
+	private String text = "Starting text";// getter + setter
+	private String metadata = null;
+	private String message = "bean message";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void setMetadata(String metadata) {
+		this.metadata = metadata;
+	}
 	
+	public String getMetadata() {
+		return this.metadata;
+	}
+
+	public void getJSON() {
+		FileInputStream inputStream;
+		try {
+
+			ClassLoader loader = Thread.currentThread().getContextClassLoader();
+			inputStream = (FileInputStream) loader
+					.getResourceAsStream("/metadata.json");
+			this.metadata = IOUtils.toString(inputStream);
+			inputStream.close();			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//return this.metadata;
+	}
+
 	public String getText() {
 		return text;
 	}
@@ -30,40 +73,43 @@ public class ButtonBean implements Serializable {
 		this.text = text;
 	}
 
-	public String javascriptCall(){
+	public String javascriptCall() {
 		return "haha";
 	}
 	
 	public void listener() {
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Calendar cal = Calendar.getInstance();
-	    text = "Text was changed via remote command on " + dateFormat.format(cal.getTime());
+		text = "Text was changed via remote command on "
+				+ dateFormat.format(cal.getTime());
 	}
-	
+
 	public String getHost(String page) {
 		try {
-			
+
 			/**
 			 * Note: this is the hard way.
 			 * 
-			 * The easy way is to simply put the page url to navigate to in the xhtml.
+			 * The easy way is to simply put the page url to navigate to in the
+			 * xhtml.
 			 */
-			
-			HttpServletRequest origRequest = 
-					(HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
-			
-			String url = origRequest.getScheme() + //eg. http
-							"://" + origRequest.getServerName() + //eg. localhost
-							":" + origRequest.getServerPort() + // eg. 8080
-							origRequest.getContextPath() + // eg. primefaces
-							origRequest.getServletPath() + // eg. faces
-							"/" + page; // eg. index.xhtml
-			
-			ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-		    //((HttpServletRequest) ec.getRequest()).logout();
-		    ec.invalidateSession();
-		    ec.redirect(url);
-			
+
+			HttpServletRequest origRequest = (HttpServletRequest) FacesContext
+					.getCurrentInstance().getExternalContext().getRequest();
+
+			String url = origRequest.getScheme() + // eg. http
+					"://" + origRequest.getServerName() + // eg. localhost
+					":" + origRequest.getServerPort() + // eg. 8080
+					origRequest.getContextPath() + // eg. primefaces
+					origRequest.getServletPath() + // eg. faces
+					"/" + page; // eg. index.xhtml
+
+			ExternalContext ec = FacesContext.getCurrentInstance()
+					.getExternalContext();
+			// ((HttpServletRequest) ec.getRequest()).logout();
+			ec.invalidateSession();
+			ec.redirect(url);
+
 			return url;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -74,6 +120,6 @@ public class ButtonBean implements Serializable {
 
 	public void setHost(String host) {
 		this.host = host;
-	}	
+	}
 
 }
